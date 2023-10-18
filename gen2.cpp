3b3a5b0d6b19c353ae0b9fc7289dcc092c662833
Beisenbek Baisakov <bbaisakov@dar.io>
2023-10-18 15:07:55 +0600

    upd

--- gen2.cpp
+++ gen2.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 void print(int n, int maxi) {
+    cout << n << endl;
     for (int i = 0; i < n; i++) {
         if (i) cout << " ";
         cout << rnd.next(maxi);
@@ -15,7 +16,7 @@ int main(int argc, char* argv[]) {
 
     registerGen(argc, argv, 1);
 
-    int n = atoi(argv[1]);
+    int n = rnd.next(atoi(argv[1]));
     int maxi = atoi(argv[2]);
 
     print(n, maxi);
