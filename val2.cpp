1809b79dec5d6890acca6286611b96f8160b475b
Beisenbek Baisakov <bbaisakov@dar.kz>
2023-11-20 11:22:57 +0600

    upd

--- val2.cpp
+++ val2.cpp
@@ -1,16 +1,17 @@
 #include "testlib.h"
-
+ 
 using namespace std;
-
+ 
 int main(int argc, char* argv[])
 {
     registerValidation(argc, argv);
     
-    int n = inf.readInt(1, 100, "n");
+    int n = inf.readInt(1, 10'000, "n");
     inf.readEoln();
     for (int i = 0; i < n; i++){
-        inf.readInt(0, 100'000, "v");
+        inf.readInt(0, 10'000, "v");
         if(i < n-1) inf.readSpace();
+        else inf.readEoln();
     }
     inf.readEof();
     return 0;
